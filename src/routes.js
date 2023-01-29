63f7208bd2072b50d736a6e87807bda5169b263b
ggkolarov <georgikollarov@gmail.com>
2023-01-29 14:50:55 +0200

    Add search functionality

--- src/routes.js
+++ src/routes.js
@@ -15,10 +15,13 @@ router.get('/', homeController.getHomePage);
 // About page
 router.get('/about', homeController.getAboutPage);
 // Create page
+router.get('/404', homeController.getErrorPage);
+
 router.get('/create', cubeController.getCreateCube); // controllers usage
 
 router.post('/create', cubeController.postCreateCube);
 
 router.get('/details/:cubeId', cubeController.getDetails);
+
 
 module.exports = router;
