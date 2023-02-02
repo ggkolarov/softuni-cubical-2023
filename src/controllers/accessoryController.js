f2cd8f80d329c7f5408975db7eea8566ae8ca2b8
ggkolarov <georgikollarov@gmail.com>
2023-02-02 09:40:57 +0200

    Create accessory add to menu

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
+const Accessory = require('../models/Accessory');
+
 //url: /accessory/create
 router.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+
+router.post('/create', async (req, res) => {
+    const {name, description, imageUrl} = req.body;
+
+    await Accessory.create({name, description, imageUrl}); // ako go suzdadesh uspeshno redirect to homepage
+
+    res.redirect('/');
 });
 
 module.exports = router;
